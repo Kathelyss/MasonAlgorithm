33a8a296f75954d4c60c9729db401aabe5503dcf
VasilevAnatoly <tolya051996@mail.ru>
2017-06-01 16:54:33 +0300

    Update

--- MasonAlgorithm/MasonAlgorithm/Program.cs
+++ MasonAlgorithm/MasonAlgorithm/Program.cs
@@ -16,6 +16,7 @@ namespace MasonAlgorithm
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
+            /////
         }
     }
 }
