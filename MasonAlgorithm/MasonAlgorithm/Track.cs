b0183268195eb4e8c52f03aff933a291a628940f
Unknown <rubicante@icloud.com>
2017-06-01 17:42:05 +0300

    add Track.cs & Vertex.cs

---
+++ MasonAlgorithm/MasonAlgorithm/Track.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MasonAlgorithm
+{
+    /// <summary>
+    /// Класс, представляющий собой отдельный путь в графе.
+    /// </summary>
+    class Track
+    {
+        /// <summary>
+        /// Вершины, которые начинаются из данного пути.
+        /// </summary>
+        public Vertex begin { get; set; }
+
+        /// <summary>
+        /// Вершины, которые заканчиваются из данного пути.
+        /// </summary>
+        public Vertex end { get; set; }
+
+        /// <summary>
+        /// Передатояная функция пути.
+        /// </summary>
+        public string PF { get; set; }
+
+        /// <summary>
+        /// Конструктор, который инициализирует начальную, конечную вершину, соединяющюю путь и ПФ пути.
+        /// </summary>
+        public Track(Vertex begin, Vertex end, string PF)
+        {
+            this.begin = begin;
+            this.end = end;
+            this.PF = PF;
+            begin.myWay.Add(this);
+            end.myWay.Add(this);
+        }
+
+        public override string ToString()
+        {
+            return begin.ToString() + " -> " + PF + " -> " + end.ToString();
+        }
+    }
+}
---
+++ MasonAlgorithm/MasonAlgorithm/Vertex.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MasonAlgorithm
+{
+    /// <summary>
+    /// Класс, представляющий вершину графа.
+    /// </summary>
+    class Vertex
+    {
+        /// <summary>
+        /// Пути из вершины.
+        /// </summary>
+        public List<Track> myWay;
+
+        /// <summary>
+        /// Возвращает все пути, входящие в вершину.
+        /// </summary>
+        public List<Track> inWay => myWay.Where((a) => a.end == this).ToList();
+
+        /// <summary>
+        /// Возвращает все пути, исходящие из данной вершины.
+        /// </summary>
+        public List<Track> OutWay => myWay.Where((a) => a.begin == this).ToList();
+
+        /// <summary>
+        /// Название верины.
+        /// </summary>
+        string Name;
+
+        public Vertex(string Name)
+        {
+            this.Name = Name;
+            myWay = new List<Track>();
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+    }
+}
