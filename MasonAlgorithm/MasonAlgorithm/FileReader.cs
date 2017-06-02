af8f54b2d108e2dcd8ffdc3d4092690b19516d48
Unknown <rubicante@icloud.com>
2017-06-02 18:49:39 +0300

    add CheckDimensions

--- MasonAlgorithm/MasonAlgorithm/FileReader.cs
+++ MasonAlgorithm/MasonAlgorithm/FileReader.cs
@@ -8,28 +8,57 @@ namespace MasonAlgorithm
         /// <summary>
         /// Чтение исходного графа из файла с разрешением .xml.
         /// </summary>
-        /// <param name="filePath">Пуль к исходному файлу (.xml) в файловой системе.</param>
+        /// <param name="filePath">Пуль к исходному файлу (.xml) в проекте (bin\Debug).</param>
         /// <returns>Возвращает полученный граф.</returns>
         public OrGraph ReadFromFile(string filePath)
         {
             //читаем данные из файла
             XDocument doc = XDocument.Load(filePath);
-            int countOfNodes = 0;
-            //проходим по каждой строке в графе
+            int countOfNodes = CheckDimensions(doc);
+            if (countOfNodes > 0)
+            {
+                // создание графа с указанным числом вершин
+                List<Vertex> nodes = new List<Vertex>();
+                for (int i = 0; i < countOfNodes; i++)
+                    nodes.Add(new Vertex(i.ToString()));                
+
+                //проходим по каждой строке в графе
+                foreach (XElement row in doc.Root.Elements())
+                {
+                    // и по каждой ячейке (PF)
+                    foreach (XElement PF in row.Elements())
+                        if (PF.Value != "0")
+                            new Track(new Vertex(row.Attribute("id").Value), new Vertex(PF.Attribute("id").Value), PF.Value);
+                }
+
+                return new OrGraph(nodes, nodes[0], nodes[nodes.Count - 1]);
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Проверка совпадения размерностей матрицы смежности графа.
+        /// </summary>
+        /// <param name="doc"></param>
+        /// <returns>Возвращает 0, если размерности не совпадают и размерность, если совпадают.</returns>
+        int CheckDimensions(XDocument doc)
+        {
+            int rows = 0;
+            // считаем количество строк
+            foreach (XElement row in doc.Root.Elements())
+                rows++;
+
+            // проверяем совпадение числа строк и столбцов
             foreach (XElement row in doc.Root.Elements())
             {
-                // и по каждой ячейке (PF)
+                int cols = 0;
                 foreach (XElement PF in row.Elements())
-                    if(PF.Value != "0")
-                        new Track(new Vertex(row.Attribute("id").Value), new Vertex(PF.Attribute("id").Value), PF.Value);
-                countOfNodes++;
+                    cols++;
+                if (rows != cols)
+                    return 0;
             }
 
-            // создание графа с указанным числом вершин
-            List<Vertex> nodes = new List<Vertex>();
-            for (int i = 0; i < countOfNodes; i++)
-                nodes.Add(new Vertex(i.ToString()));
-            return new OrGraph(nodes, nodes[0], nodes[nodes.Count - 1]);
+            return rows;
         }
     }
 }
